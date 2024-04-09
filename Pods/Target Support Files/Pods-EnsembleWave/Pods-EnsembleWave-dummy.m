c83a3b59d1b7a9a3c921475191e8269c87e06f64
Min <62638850+lebonthe@users.noreply.github.com>
2024-04-09 15:19:23 +0800

    Add SwiftLint

---
+++ Pods/Target Support Files/Pods-EnsembleWave/Pods-EnsembleWave-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EnsembleWave : NSObject
+@end
+@implementation PodsDummy_Pods_EnsembleWave
+@end
---
+++ Pods/Target Support Files/Pods-EnsembleWaveTests/Pods-EnsembleWaveTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EnsembleWaveTests : NSObject
+@end
+@implementation PodsDummy_Pods_EnsembleWaveTests
+@end
