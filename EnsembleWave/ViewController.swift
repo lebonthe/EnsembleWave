9755525c2ad117524274d9448deb312e22c5a9d1
Min <62638850+lebonthe@users.noreply.github.com>
2024-04-09 13:58:24 +0800

    Initial Commit

---
+++ EnsembleWave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnsembleWave
+//
+//  Created by Min Hu on 2024/4/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
