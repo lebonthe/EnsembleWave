7d3667c3c9703b1d6572bde042f91d85af00c10a
Min <62638850+lebonthe@users.noreply.github.com>
2024-04-09 23:38:17 +0800

    Fix: Show tab bar in recording home page

---
+++ EnsembleWave/Recording Page/RecordingHomePageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RecordingHomePageViewController.swift
+//  EnsembleWave
+//
+//  Created by Min Hu on 2024/4/9.
+//
+
+import UIKit
+
+class RecordingHomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
