c7dde95b2389a0f505a34a77006eaf6c62413e57
Min <62638850+lebonthe@users.noreply.github.com>
2024-04-26 17:51:49 +0800

    Feature: Signin with AppleID and Firebase

---
+++ EnsembleWave/Controller/CustomFunc.swift
@@ -0,0 +1,26 @@
+//
+//  CustomFunc.swift
+//  EnsembleWave
+//
+//  Created by Min Hu on 2024/4/26.
+//
+
+import Foundation
+import UIKit
+
+class CustomFunc {
+    /// 提示框
+    /// - Parameters:
+    ///   - title: 提示框標題
+    ///   - message: 提示訊息
+    ///   - vc: 要在哪一個 UIViewController 上呈現
+    ///   - actionHandler: 按下按鈕後要執行的動作，沒有的話，就填 nil
+    class func customAlert(title: String, message: String, vc: UIViewController, actionHandler: (() -> Void)?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let closeAction = UIAlertAction(title: "關閉", style: .default) { action in
+            actionHandler?()
+        }
+        alertController.addAction(closeAction)
+        vc.present(alertController, animated: true)
+    }
+}
