d6da647783565eba575c34535a3a85615183fa55
Min <62638850+lebonthe@users.noreply.github.com>
2024-04-30 18:52:41 +0800

    Feature: Replan login, logout and account deletion method

--- EnsembleWave/Controller/ProfileViewController.swift
+++ EnsembleWave/Controller/ProfileViewController.swift
@@ -11,10 +11,6 @@ import CryptoKit
 import AuthenticationServices
 
 class ProfileViewController: UIViewController {
-
-    @IBOutlet weak var signOutButton: UIBarButtonItem!
-    
-    @IBOutlet weak var signInButton: UIBarButtonItem!
     
     let deleteAccountButton: UIButton = {
        let button = UIButton()
@@ -24,98 +20,59 @@ class ProfileViewController: UIViewController {
     fileprivate var currentNonce: String?
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        setupUI()
+
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        checkLoginStatus()
         print("viewWillAppear")
+
+        checkLoginStatus()
+
     }
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         print("viewWillLayoutSubviews")
     }
-    func updateUI() {
-        if Auth.auth().currentUser != nil {
-            deleteAccountButton.setTitle("刪除帳號", for: .normal)
-            //         deleteAccountButton.backgroundColor = .yellow
-            view.addSubview(deleteAccountButton)
-            deleteAccountButton.addTarget(self, action: #selector(deleteCurrentUser), for: .touchUpInside)
-            NSLayoutConstraint.activate([
-                deleteAccountButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                deleteAccountButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-                deleteAccountButton.widthAnchor.constraint(equalToConstant: 80),
-                deleteAccountButton.heightAnchor.constraint(equalToConstant: 30)
-            ])
-        } else {
-            
-        }
-    }
+
     // TODO: 繼續刪除流程，並把user資料串接，在 Profile 頁面顯示創作與名字
    
-    @objc func deleteCurrentUser() {
-        do {
-            let nonce = try CryptoUtils.randomNonceString()
-            currentNonce = nonce
-            let appleIDProvider = ASAuthorizationAppleIDProvider()
-            let request = appleIDProvider.createRequest()
-            request.requestedScopes = [.fullName, .email]
-            request.nonce = CryptoUtils.sha256(nonce)
-            
-            let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-            authorizationController.delegate = self
-            authorizationController.presentationContextProvider = self
-            authorizationController.performRequests()
-        } catch {
-            // In the unlikely case that nonce generation fails, show error view.
-            displayError(error)
-        }
-    }
     
-    @IBAction func signOut(_ sender: Any) {
-        let firebaseAuth = Auth.auth()
-        do {
-            try firebaseAuth.signOut()
-            CustomFunc.customAlert(title: "已成功登出", message: "", vc: self) {
-                print("sign out")
-                if #available(iOS 16.0, *) {
-                    self.signOutButton.isHidden = true
-                    self.signInButton.isHidden = false
-                } else {
-                    self.navigationItem.rightBarButtonItem = self.signInButton
-                }
-                self.checkLoginStatus()
-            }
-        } catch let signOutError as NSError {
-            print("Error signing out: %@", signOutError)
-        }
+    func setupLoginButton() {
+        let loginButton = UIButton()
+         loginButton.translatesAutoresizingMaskIntoConstraints = false
+         loginButton.addTarget(self, action: #selector(presentLoginViewController), for: .touchUpInside)
+         view.addSubview(loginButton)
+        loginButton.setTitle("登入", for: .normal)
+        loginButton.setTitleColor(.white, for: .normal)
+     NSLayoutConstraint.activate([
+         loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+     ])
+    }
+    func setupSettingButton() {
+        let settingButton = UIBarButtonItem(title: "設定", style: .plain, target: self, action: #selector(pushSettingViewController))
+        navigationItem.rightBarButtonItem = settingButton
+    }
+    @objc func pushSettingViewController() {
+        let settingViewController = SettingViewController()
+        navigationController?.pushViewController(settingViewController, animated: true)
     }
     func checkLoginStatus() {
         if Auth.auth().currentUser == nil {
-            if #available(iOS 16.0, *) {
-                signOutButton.isHidden = true
-                signInButton.isHidden = false
-            } else {
-                self.navigationItem.rightBarButtonItem = signInButton
-            }
             for subview in view.subviews {
                 subview.removeFromSuperview()
             }
-          
-//            presentLoginViewController()
+            setupLoginButton()
+            navigationItem.rightBarButtonItem = nil
         } else {
-            if #available(iOS 16.0, *) {
-                signOutButton.isHidden = false
-                signInButton.isHidden = true
-            } else {
-                self.navigationItem.rightBarButtonItem = signOutButton
+            for subview in view.subviews {
+                subview.removeFromSuperview()
             }
-            updateUI()
+            setupSettingButton()
             print("=========Already Login")
         }
     }
-    @IBAction func presentLoginViewController() {
+    @objc func presentLoginViewController() {
         let loginViewController = LoginViewController()
         loginViewController.delegate = self
 //        loginViewController.modalPresentationStyle = .fullScreen
@@ -129,63 +86,4 @@ extension ProfileViewController: LoginViewControllerDelegate {
     }
 }
 
-extension ProfileViewController: ASAuthorizationControllerPresentationContextProviding {
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        return self.view.window!
-    }
-    
-    func displayError(_ error: Error) {
-        DispatchQueue.main.async {
-            let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default))
-            self.present(alert, animated: true)
-        }
-    }
 
-}
-
-extension ProfileViewController: ASAuthorizationControllerDelegate {
-
-    func authorizationController(controller: ASAuthorizationController,
-                                 didCompleteWithAuthorization authorization: ASAuthorization) {
-      guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential
-      else {
-        print("Unable to retrieve AppleIDCredential")
-        return
-      }
-
-      guard let _ = currentNonce else {
-        fatalError("Invalid state: A login callback was received, but no login request was sent.")
-      }
-
-      guard let appleAuthCode = appleIDCredential.authorizationCode else {
-        print("Unable to fetch authorization code")
-        return
-      }
-
-      guard let authCodeString = String(data: appleAuthCode, encoding: .utf8) else {
-        print("Unable to serialize auth code string from data: \(appleAuthCode.debugDescription)")
-        return
-      }
-
-      Task {
-        do {
-          try await Auth.auth().revokeToken(withAuthorizationCode: authCodeString)
-            let user = Auth.auth().currentUser
-          try await user?.delete()
-            CustomFunc.customAlert(title: "使用者帳號已刪除", message: "", vc: self) {
-                self.checkLoginStatus()
-            }
-          
-        } catch {
-          self.displayError(error)
-        }
-      }
-    }
-
-  func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-    // Handle error.
-    print("Sign in with Apple errored: \(error)")
-  }
-
-}
